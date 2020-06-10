f5465971701bb9524bb76e2b5edb2921fce9b611
deepak-km <dkmiitg@gmail.com>
2020-06-10 17:27:27 +0530

    Create read_write.py

---
+++ read_write.py
@@ -0,0 +1,18 @@
+#-------------------------Reading & Writing data in Files----------------------
+# Reading CSV Files with Pandas:
+import pandas
+df = pandas.read_csv('F:/pyWork/pyData/User_Data.csv')
+print(df)
+
+# Writing CSV Files with Pandas:
+import pandas
+df.to_csv('F:/pyWork/pyData/User_Data_1.csv')
+
+# Reading Excel Files with Pandas
+df = pandas.read_excel('F:/pyWork/pyData/User_Data.xlsx')
+print(df)
+
+# Writing Excel Files with Pandas 
+df1 = pandas.DataFrame(df)
+print (df1)
+df1.to_excel('Bank1.xlsx')
