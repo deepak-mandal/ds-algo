7026265eeb6c7b412079402d918af37d65e4e2be
deepak-km <dkmiitg@gmail.com>
2020-07-08 22:02:05 +0530

    Create df.py

---
+++ Analytics/df.py
@@ -0,0 +1,8 @@
+import pandas as pd
+d = {'name':pd.Series(['Alisa','Bobby','Cat','madan','rocky','jocky','jack','rahul','david','ajay']),
+   'age':pd.Series([26,27,28,19,64,65,14,58,25,12]),
+   'score':pd.Series([89,87,90,96,99,70,71,45,54,58])}
+df=pd.DataFrame(d)      #Creating Data frame
+print(df)
+
+print(df.describe())    #Getting Data summerisation such as mean, std etc.
