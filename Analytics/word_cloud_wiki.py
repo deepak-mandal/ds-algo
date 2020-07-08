9d4872aeac9127b622a3af576d7f006ab4b0b744
deepak-km <dkmiitg@gmail.com>
2020-07-09 02:01:53 +0530

    Update word_cloud_wiki.py

--- Analytics/word_cloud_wiki.py
+++ Analytics/word_cloud_wiki.py
@@ -1,8 +1,8 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 22 05:23:42 2020
-
 @author: deepak
 """
 
@@ -15,7 +15,7 @@ def get_wiki(query):
     page=wikipedia.page(title)
     return page.content
 
-print(get_wiki('python programming language'))
+print(get_wiki('data analysis'))
 
 from wordcloud import WordCloud, STOPWORDS
 
@@ -26,7 +26,7 @@ wc=WordCloud(width=800, height=800,
              mask=None,
              max_words=200,
              stopwords=stopword,
-             min_font_size=10).generate(get_wiki('python programming language'))
+             min_font_size=10).generate(get_wiki('data analysis'))
              
 
-wc.to_file('wc4.png')
+wc.to_file('wc.png')
