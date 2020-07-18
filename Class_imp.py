0e8af7fc72519bbfe7664cd13443732a7f8a8118
Deepak <dkmiitg@gmail.com>
2020-07-18 09:01:48 +0530

    Update Class_imp.py

--- Class_imp.py
+++ Class_imp.py
@@ -8,15 +8,27 @@ Created on Thu Jul  9 12:13:44 2020
 
 
 class person:
-    counter=0     #------------------------------------------------------------Class Attribute
-    def __init__(self, a='Ravi', b='Ravi'):       #----------------------------Instance method or methods
-        self.name=a           #------------------------------------------------Attributes or Instance Variables
-        self.gender=b            #---------------------------------------------Attribute or Instance variable
+    counter=0       #------------------------------------------------------class attribute
+    def __init__(self, a, b):         #--------------------Instance methods/methods 
+        self.name=a        #---------------------------------------------------Attributes/Instance variable
+        self.gender=b      #--------------------------------------------Attributes/Instance variable
         person.counter=person.counter+1
-    def ShowInfo(self):           #--------------------------------------------Instance method or methods
+    def ShowInfo(self):         #---------------------------------------Instance methods/methods
         print('Name: ', self.name)
-        print('Gender', self.gender)
+        print('Gender: ', self.gender)
+        print()
     @classmethod
-    def ShowCount(cls):         #----------------------------------------------Class method
-        print('Number of object created so far: ', cls.counter)
+    def ShowCount(cls):         #----------------------------------------------class method
+        print('Number of objects created so far: ', cls.counter)
+        print()
         
+        
+        
+p1=person('Deepak', 'Male')
+p2=person('Matt', 'Male')
+p3=person('Sophie', 'Female')
+
+person.ShowCount()
+p1.ShowInfo()
+p2.ShowInfo()
+p3.ShowInfo()
