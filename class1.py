11ad222b62ffc36257557b2f700fb0cc87f0fa96
Deepak <dkmiitg@gmail.com>
2020-07-18 09:00:52 +0530

    Update class1.py

--- class1.py
+++ class1.py
@@ -7,11 +7,20 @@ Created on Thu Jul  9 11:06:15 2020
 """
 
 
+
 class person:
-    def __init__(self, a='Ravi', b='Male'):
+    def __init__(self, a, b):
         self.name=a
         self.gender=b
     def ShowInfo(self):
         print('Name: ', self.name)
         print('Gender: ', self.gender)
         
+     
+p1=person('Sophie', 'Female')
+p2=person('Mark', 'Male')
+
+p1.ShowInfo()
+p2.ShowInfo()
+
+
