5e1563f83e1410e7c5cfdd0cc130cded334bbb66
Deepak <dkmiitg@gmail.com>
2020-07-27 12:05:42 +0530

    Create reverse_list.py

---
+++ ds_algo/reverse_list.py
@@ -0,0 +1,11 @@
+    
+    def reverse_list(self):
+        prev=None
+        p=self.start
+        while p is not None:
+            next=p.link
+            p.link=prev
+            prev=p
+            p=next
+        self.start=prev
+  
