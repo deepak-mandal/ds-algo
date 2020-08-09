5f252560154c208836e6a54d1fa2bbf190fa71d9
Deepak <dkmiitg@gmail.com>
2020-08-09 17:27:09 +0530

    Delete count_node_of_list.py

--- ds_algo/count_node_of_list.py
+++
@@ -1,8 +0,0 @@
-def count_nodes(self):
-        p=self.start
-        n=0
-        while p is not None:
-            n+=1
-            p=p.link
-        print('Number of nodes in the list = ', n)
-    
