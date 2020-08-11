a6c8c428ace5cf4aa4a9390ebe673be91c6193c9
Deepak <dkmiitg@gmail.com>
2020-08-11 13:17:50 +0530

    Create funct.cpp

---
+++ funct.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+void message();
+using namespace std;
+int main()
+{
+message();
+cout<<"Cry and you stop the monotony!\n";
+message();
+}
+void message()
+{
+cout<<"Smile and the world smiles with you..\n";
+}
