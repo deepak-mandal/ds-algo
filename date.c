88a57ca543cc542edb5a3de8a4c9b189cea17833
Deepak <dkmiitg@gmail.com>
2020-08-11 13:15:48 +0530

    Create date.c

---
+++ date.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+
+int day, month, year;
+printf("Enter date : ");
+scanf ("%d/%d/%d", &day, &month, &year );
+printf("%d%d%d\n", day, month, year);
+
+
+
+
+
+
+return 0;
+}
