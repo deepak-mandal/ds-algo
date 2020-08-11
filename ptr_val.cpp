f3d14457bfd7c317bd33210e7a63d4e58e3eca5b
Deepak <dkmiitg@gmail.com>
2020-08-11 13:13:35 +0530

    Create ptr_val.cpp

---
+++ ptr_val.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i = 3;
+int *j;
+j = &i;
+
+cout<<"Address of i = "<<&i<<endl;
+cout<<"Address of i = "<<j<<endl;
+cout<<"Address of j = "<<&j<<endl;
+cout<<"Value of j = "<<j<<endl;
+cout<<"Value of i = "<<i<<endl;
+cout<<"Value of i = "<<*j<<endl;
+cout<<"Value of i = "<<*(&i)<<endl;
+
+
+}
