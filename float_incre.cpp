76eb2c963e573a6283144aff23f5bde8503f9bb5
Deepak <dkmiitg@gmail.com>
2020-08-11 12:45:15 +0530

    Create float_incre.cpp

---
+++ float_incre.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+float i = 1.5;
+while(i<7)
+{
+cout<<"Hello, I'm Deepak Mandal from IIT Guwahati"<<endl;
+cout<<"I'm happy to see you here!"<<endl;
+i=i+.5;
+
+}
+
+
+}
