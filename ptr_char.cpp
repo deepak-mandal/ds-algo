b886b202d43ee1bbae2edca03fefc5d6842936cc
Deepak <dkmiitg@gmail.com>
2020-08-11 12:57:36 +0530

    Create ptr_char.cpp

---
+++ ptr_char.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+char str1[] = "Hello";
+char str2[10];
+//char *s = "Good Morning";
+char *q;
+//str2 = str1;
+q = str1;
+//cout<<str2<<endl;
+cout<<q<<endl;
+
+}
