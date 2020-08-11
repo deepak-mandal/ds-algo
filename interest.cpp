4c0086e9dad501480ae9418ba2960708250e0a48
Deepak <dkmiitg@gmail.com>
2020-08-11 12:42:59 +0530

    Create interest.cpp

---
+++ interest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+float p, n, r, si;
+int i = 1;
+for(i = 1; i<=3; i++)
+{
+cout<<"Enter value of p, n and r respectively : \n";
+cin>>p>>n>>r;
+si = p*n*r/100;
+cout<<"si = "<<si<<endl;
+
+}
+
+}
