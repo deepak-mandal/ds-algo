012ca2db066a19a713a7139b9fc0e6c99723510e
Deepak <dkmiitg@gmail.com>
2020-08-11 14:11:50 +0530

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main()
+{
+int i = 2;
+switch(i)
+{
+	case 1:
+	cout<<"i am in case 1\n";
+	case 2:
+	cout<<"i'm in case 2\n";
+	case 3:
+	cout<<"I am in case 3\n";
+	default :
+	cout<<"I am in default\n";
+}
+
+}
