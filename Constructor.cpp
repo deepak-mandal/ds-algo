04293d31722123bb3ac18d567c156bb75bea418c
Deepak <dkmiitg@gmail.com>
2020-08-11 12:32:25 +0530

    Create Constructor.cpp

---
+++ Constructor.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+//creating class
+class square{
+public:
+int side;
+square()		//creating constructor of class square which doesn't have any return type
+{
+side = 20;
+}
+
+};
+
+main()
+{
+//creating object
+square c;
+cout<<c.side<<endl;
+
+
+
+}
