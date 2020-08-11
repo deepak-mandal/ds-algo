8347438c607cb2eba9ae3857cfc3010abe75b847
Deepak <dkmiitg@gmail.com>
2020-08-11 13:48:28 +0530

    Create math_conio.c

---
+++ math_conio.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+
+{
+float x;
+printf("Enter the value of x to find the value of sin(x)\n");
+
+scanf("%f",&x);
+
+printf("sin(x) = %f\n",sin(x));
+
+
+return 0;
+}
