438625ef6d19c1f4e2e787b910c45b6032e61c37
Deepak <dkmiitg@gmail.com>
2020-08-11 12:43:44 +0530

    Create func_decl.cpp

---
+++ func_decl.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void message();		//function prototype declaration
+main()
+{
+for(int i=0; i<=10; i++)
+{
+message();		//function call
+
+cout<<"cry and you stop the monotony!\n";
+}
+}
+void message()		//function definition
+{
+cout<<"Smile and the world smiles with you...\n";
+
+}
