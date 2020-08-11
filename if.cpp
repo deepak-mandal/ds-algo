800291a89c7a69a2560f1d696b0ab4ead30b977b
Deepak <dkmiitg@gmail.com>
2020-08-11 12:55:05 +0530

    Create if.cpp

---
+++ if.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int num;
+cout<<"enter a number less than 10 : ";
+cin>>num;
+if(num<10)
+cout<<"entered number is less than 10"<<endl;
+else
+cout<<"you haven't entered right number, please enter correct number "<<endl;
+
+}
