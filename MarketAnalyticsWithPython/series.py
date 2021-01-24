6a64183e806ea669b108856b93d6b7dcaab20df0
deepak-mandal <dkmiitg@gmail.com>
2021-01-24 15:12:16 +0530

    Create series.py

---
+++ MarketAnalyticsWithPython/series.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 10:44:14 2021
+
+@author: deepak
+"""
+
+import pandas as pd
+obj=pd.Series([1, 24, 34, 35, 23, 3, 2, 55])
+print(obj)
+print(obj.values)
+print(obj.index)
