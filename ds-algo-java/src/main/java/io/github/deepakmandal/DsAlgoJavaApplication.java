c8a526e6c20492ce051c1a38606f6415d40b5580
Deepak <dkmiitg@gmail.com>
2022-09-15 20:51:37 +0530

    ds algo in java

--- ds-algo-java/src/main/java/io/github/deepakmandal/DsAlgoJavaApplication.java
+++ ds-algo-java/src/main/java/io/github/deepakmandal/DsAlgoJavaApplication.java
@@ -1,7 +1,11 @@
 package io.github.deepakmandal;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.deepakmandal.sorting.SelectionSort;
 
 @SpringBootApplication
 public class DsAlgoJavaApplication {
@@ -9,6 +13,23 @@ public class DsAlgoJavaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DsAlgoJavaApplication.class, args);
 		System.out.println("Data structure and algorithms in Java");
+		
+		SelectionSortAlgorithm();
+		
+		
+		
 	}
+
+	private static void SelectionSortAlgorithm() {
+		
+		int unSortedList[] = new int[] {40, 50, 6, 2, 95, 1001, 565};
+		System.out.println(Arrays.toString(unSortedList));
+		SelectionSort selectionSort = new SelectionSort();
+		selectionSort.selectionSort(unSortedList);
+		
+	}
+	
+	
+	
 
 }
