f27152f3f5cd2ecac4c498ffb125bac508ecee59
Deepak <dkmiitg@gmail.com>
2022-09-15 20:30:41 +0530

    ds algo in java

---
+++ ds-algo-java/src/main/java/io/github/deepakmandal/DsAlgoJavaApplication.java
@@ -0,0 +1,14 @@
+package io.github.deepakmandal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAlgoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAlgoJavaApplication.class, args);
+		System.out.println("Data structure and algorithms in Java");
+	}
+
+}
---
+++ ds-algo-java/src/test/java/io/github/deepakmandal/DsAlgoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.deepakmandal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAlgoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
