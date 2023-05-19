3f304abca5c27bc2990700e3ad808ce6b97778a6
Deepak <dkmiitg@gmail.com>
2023-05-19 23:12:27 +0530

    Angular + Spring + SQL + NoSQL

--- SoulmateApplication/soulmate-service/src/main/java/com/stackroute/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stackroute.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.stackroute.model.User;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-
-}
